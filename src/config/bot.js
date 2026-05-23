9983200fc3ea13afd6b503f389221eb4a02f4e2c
david12345-beep <david9128.mogilko@gmail.com>
2026-05-23 23:07:21 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made for games club",
+        name: "Made for nigers club",
         // Activity type number (0 = Playing).
         type: 0, 
       },
